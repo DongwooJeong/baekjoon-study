01ec15e9d4b86695ca643df6570d6d39f341a797
unknown <dongwoo.jeong@outlook.com>
2021-11-30 07:00:41 -0500

    add sorting10

---
+++ sorting/18870.py
@@ -0,0 +1,9 @@
+N=int(input())
+num=list(map(int,input().split()))
+
+num2=sorted(list(set(num)))
+dictnum={num2[i]: i for i in range(len(num2))}
+print(num2)
+print(dictnum)
+for i in num:
+    print(dictnum[i], end=' ')
